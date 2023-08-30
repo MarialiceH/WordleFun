5a5f1c38e83b39dc81ce86270e560e67e68a882b
MarialiceH <137061846+MarialiceH@users.noreply.github.com>
2023-08-30 16:50:15 -0400

    Corrected JavaScript

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const keys = document.querySelectorAll(".keyboard-row button");
 
     for (let i = 0; i < keys.length; i++) {
-        keys[i].onClick = ({ target }) => {
-            const key = target.getAttribute("data-key");
+        keys[i].onclick = ({ target }) => {
+          const key = target.getAttribute("data-key");
 
-            console.log(key);
+          console.log(key);
         };
     }
+
 
     function createSquares() {
         const gameBoard = document.getElementById("board");
 
-        for (let index = 0; index < 30; index++) {
+        for (let index = 0; index < 30; index++){
             let square = document.createElement("div");
             square.classList.add("square");
             square.setAttribute("id", index + 1);
             gameBoard.appendChild(square);
         }
     }
+
+
 });
