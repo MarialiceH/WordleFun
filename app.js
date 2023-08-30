f63042ddf2e729e2e870cefd2d04326b220f9f03
MarialiceH <137061846+MarialiceH@users.noreply.github.com>
2023-08-30 14:47:23 -0400

    Updated typo

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    createSquares()
+    createSquares();
 
-    const keys = document.querySelectorAll('keyboard-row button')
+    const keys = document.querySelectorAll(".keyboard-row button");
 
     for (let i = 0; i < keys.length; i++) {
         keys[i].onClick = ({ target }) => {
             const key = target.getAttribute("data-key");
-            
-            console.log(key)
+
+            console.log(key);
         };
     }
 
     function createSquares() {
-        const gameBoard = document.getElementById("board")
+        const gameBoard = document.getElementById("board");
 
         for (let index = 0; index < 30; index++) {
             let square = document.createElement("div");
             square.classList.add("square");
             square.setAttribute("id", index + 1);
             gameBoard.appendChild(square);
-
         }
     }
-
-
-})
+});
