6abc42b5c72346d71125107bbe1a95b56d85a024
MarialiceH <137061846+MarialiceH@users.noreply.github.com>
2023-08-31 16:25:17 -0400

    Updated flips for letters

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let index = 0; index < 30; index++){
             let square = document.createElement("div");
             square.classList.add("square");
+            square.classList.add("animate__animated");
             square.setAttribute("id", index + 1);
             gameBoard.appendChild(square);
         }
